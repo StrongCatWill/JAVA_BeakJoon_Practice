5a48e96d22e156be10435297e651a2e3680df00f
StrongCatWill <86825884+StrongCatWill@users.noreply.github.com>
2022-09-28 23:52:10 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       
+       String arr = sc.nextLine();
+       System.out.println(arr+"??!");
+       sc.close();
+    }
+}
