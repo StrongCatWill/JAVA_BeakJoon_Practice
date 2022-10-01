b74180e225b289ce5571359fcee4e84f88f34b57
StrongCatWill <86825884+StrongCatWill@users.noreply.github.com>
2022-10-01 23:56:37 +0900

    [Bronze II] Title: 벌집, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+		int count = 1; // 겹 수(최소 루트)
+		int range = 2;	// 범위 (최솟값 기준) 
+ 
+		if (N == 1) {
+			System.out.print(1);
+		}
+ 
+		else {
+			while (range <= N) {	
+				range = range + (6 * count);	 
+				count++;	 
+			}
+			System.out.print(count);
+		}
+	}
+}
