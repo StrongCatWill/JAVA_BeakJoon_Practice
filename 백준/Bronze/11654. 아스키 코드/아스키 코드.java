38882ecb420408966a9869a49f8279c30fa580ab
StrongCatWill <86825884+StrongCatWill@users.noreply.github.com>
2022-10-08 19:19:34 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+
+public class Main  {
+    public static void main(String[] args) throws IOException {
+        
+        InputStream in = System.in;
+        
+        int a;
+        a = in.read();
+        
+        System.out.println(a);
+    }
+}
+
